50e43926d939e3c58c74e7343a6ea163e8bd585b
Chaitu <chaitu.ca539@gmail.com>
2021-03-23 17:13:43 +0530

    create custom hook form froms use

--- src/pages/order.js
+++ src/pages/order.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import SEO from '../components/SEO';
+import useForm from '../utils/useForm';
 
 function OrderPage() {
+  const { values, updateValue } = useForm({
+    name: '',
+    email: '',
+  });
   return (
     <>
       <SEO title="Order a Pizza!" />
-      <div>Order Page</div>
+      <form action="">
+        <fieldset>
+          <legend>Your Info</legend>
+          <label htmlFor="name">
+            Name
+            <input
+              type="text"
+              name="name"
+              value={values.name}
+              onChange={updateValue}
+            />
+          </label>
+          <label htmlFor="email">
+            Email
+            <input
+              type="email"
+              name="email"
+              value={values.email}
+              onChange={updateValue}
+            />
+          </label>
+        </fieldset>
+        <fieldset>
+          <legend>Menu</legend>
+        </fieldset>
+        <fieldset>
+          <legend>Order</legend>
+        </fieldset>
+      </form>
     </>
   );
 }
