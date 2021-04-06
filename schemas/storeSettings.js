d0468d2b83f0f49dc9bf5a747af4da1c98abbe72
Chaitu <chaitu.ca539@gmail.com>
2021-04-07 00:59:51 +0530

    add a storesettings schema and customize sidebar

---
+++ schemas/storeSettings.js
@@ -0,0 +1,30 @@
+import { MdStore as icon } from 'react-icons/md';
+
+export default {
+  // computer name
+  name: 'storeSettings',
+  // visible title
+  title: 'Settings',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Store Name',
+      type: 'string',
+      description: 'Name of the pizza',
+    },
+    {
+      name: 'slicemaster',
+      title: 'Slicemasters currently slicing',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'person' }] }],
+    },
+    {
+      name: 'hotSlices',
+      title: 'Hotslices available in the case',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'pizza' }] }],
+    },
+  ],
+};
