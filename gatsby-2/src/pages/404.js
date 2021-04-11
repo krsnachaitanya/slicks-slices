89879576f9a6d7c681f14e8b0744ca7a2cc88729
Chaitu <chaitu.ca539@gmail.com>
2021-04-11 23:40:07 +0530

    finish merging gatsby and sanity in to master gatsby parent folder

--- gatsby-2/src/pages/404.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function FourOFourPage() {
-  return <p>Hey that page doesn't exist</p>;
-}
-
-export default FourOFourPage;
--- gatsby-2/src/utils/formatMoney.js
+++
@@ -1,9 +0,0 @@
-const formatter = Intl.NumberFormat('en-IN', {
-  style: 'currency',
-  currency: 'INR',
-  minimumFractionDigits: 0,
-});
-
-export default function formatMoney(rupees) {
-  return formatter.format(rupees);
-}
